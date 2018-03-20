13d88578040345b0cc2d3e600ba23035604405d1
Choco <i.jimenezpi@gmail.com>
2018-03-20 18:01:47 +0100

    deprecated Backtick removal

--- zmovetorrent.sh
+++ zmovetorrent.sh
@@ -7,16 +7,16 @@ echo [$(date)] " movetorrent.sh started"
 # use transmission-remote to get torrent list from transmission-remote list
 # use sed to delete first / last line of output, and remove leading spaces
 # use cut to get first field from each line
-TORRENTLIST=`transmission-remote --auth $auth --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+TORRENTLIST=$(transmission-remote --auth $auth --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1)
 # for each torrent in the list
 for TORRENTID in $TORRENTLIST
 do
-TORRENTID=`echo "$TORRENTID" | sed 's:*::'`
+TORRENTID=$(echo "$TORRENTID" | sed 's:*::')
 echo [$(date)] " Torrent #$TORRENTID: Operations on torrent starting"
 # check if torrent download is completed
-DL_COMPLETED=`transmission-remote --auth $auth --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+DL_COMPLETED=$(transmission-remote --auth $auth --torrent $TORRENTID --info | grep "Percent Done: 100%")
 # check torrent.s current state is .Stopped., .Finished., or .Idle.
-STATE_STOPPED=`transmission-remote --auth $auth --torrent $TORRENTID --info | grep "State: Stopped\|Finished\|Idle\|Seeding"`
+STATE_STOPPED=$(transmission-remote --auth $auth --torrent $TORRENTID --info | grep "State: Stopped\|Finished\|Idle\|Seeding")
 # if the torrent is .Stopped., .Finished., or .Idle. after downloading 100%.
 if [ "$DL_COMPLETED" != "" ] && [ "$STATE_STOPPED" != "" ]; then
 # move the files and remove the torrent from Transmission
