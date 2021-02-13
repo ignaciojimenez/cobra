d666a2c31161c96400b56c6a238a1ddfb38cb8f8
Ignacio Jimenez <i.jimenezpi@gmail.com>
2021-02-13 22:22:53 +0100

    Deleted really old unneded files

--- OLD/ztestusb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if grep -qs 'mnt/almacenNTFS' /proc/mounts; then
-   : 	
-   #echo [`date`] " mnt/almacenNTFS montado"
-else
-    echo [$(date)] " ERROR: mnt/almacenNTFS nomontado"
-    umount -a
-    mount -a
-fi
